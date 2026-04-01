eaf371b870ce6a9c120e15e62fa65c94bd28cd22
willrain <ruslanshambiloff@yandex.ru>
2026-04-01 20:17:45 +0300

    init front

---
+++ frontend/src/lib/leaflet.ts
@@ -0,0 +1,8 @@
+import L from 'leaflet'
+import iconUrl from 'leaflet/dist/images/marker-icon.png'
+import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png'
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png'
+
+delete (L.Icon.Default.prototype as any)._getIconUrl
+
+L.Icon.Default.mergeOptions({ iconUrl, iconRetinaUrl, shadowUrl })
